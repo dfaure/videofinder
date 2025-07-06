633d3f39c2a81b13be7f373d276b92d2d3b1f35f
David Faure <faure@kde.org>
2025-07-06 14:10:11 +0200

    Add sqlite test code, works

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,51 @@ mod simple_log;
 
 slint::include_modules!();
 
+use rusqlite::{Connection, Result};
+
+#[derive(Debug)]
+struct Person {
+    id: i32,
+    name: String,
+    data: Option<Vec<u8>>,
+}
+
+fn sqlite_test() -> Result<()> {
+    let conn = Connection::open_in_memory()?;
+
+    conn.execute(
+        "CREATE TABLE person (
+            id    INTEGER PRIMARY KEY,
+            name  TEXT NOT NULL,
+            data  BLOB
+        )",
+        (), // empty list of parameters.
+    )?;
+    let me = Person {
+        id: 0,
+        name: "Steven".to_string(),
+        data: None,
+    };
+    conn.execute(
+        "INSERT INTO person (id, name, data) VALUES (?1, ?2, ?3)",
+        (&me.id, &me.name, &me.data),
+    )?;
+
+    let mut stmt = conn.prepare("SELECT id, name, data FROM person")?;
+    let person_iter = stmt.query_map([], |row| {
+        Ok(Person {
+            id: row.get(0)?,
+            name: row.get(1)?,
+            data: row.get(2)?,
+        })
+    })?;
+
+    for person in person_iter {
+        log!("Found person {:?}", person.unwrap());
+    }
+    Ok(())
+}
+
 #[unsafe(no_mangle)]
 fn android_main(app: slint::android::AndroidApp) -> Result<(), Box<dyn Error>> {
 
@@ -14,6 +59,7 @@ fn android_main(app: slint::android::AndroidApp) -> Result<(), Box<dyn Error>> {
     slint::android::init(app).unwrap();
 
     log!("slint::android initialized");
+    sqlite_test()?;
 
     let ui = AppWindow::new()?;
 
