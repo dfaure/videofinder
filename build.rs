ecd31a4a1d4a6d5c1b708758baca2569b17f14fc
David Faure <faure@kde.org>
2025-07-14 13:34:17 +0200

    more styling

--- build.rs
+++ build.rs
@@ -1,3 +1,11 @@
 fn main() {
-    slint_build::compile("ui/app-window.slint").expect("Slint build failed");
+    let config = slint_build::CompilerConfiguration::new()
+    .with_style("fluent-light".into());
+
+    // material-light: too big
+    // fluent-light: clean, very sqaure, blue highlight below the lineedit, blue selection
+    // cupertino-light: even smaller, clean too, blue highlight around the lineedit
+    // cosmic-light: too gray
+
+    slint_build::compile_with_config("ui/app-window.slint", config).expect("Slint build failed");
 }
