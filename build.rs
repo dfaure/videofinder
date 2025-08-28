14d8f429e56433d36febd9cb85b4be58e2ae2c8f
David Faure <faure@kde.org>
2025-08-28 19:50:51 +0200

    Revert "Large Icon Button, forked from slint's Fluent button.slint"
    This reverts commit 6ec86ec5aaed0497c5e1b809cd23ef3916d64378.
    I'll do this upstream instead.

--- build.rs
+++ build.rs
@@ -2,12 +2,9 @@ fn main() {
     let config = slint_build::CompilerConfiguration::new().with_style("fluent-light".into());
 
     // material-light: too big
-    // fluent-light: clean, very square, blue highlight below the lineedit, blue selection
+    // fluent-light: clean, very sqaure, blue highlight below the lineedit, blue selection
     // cupertino-light: even smaller, clean too, blue highlight around the lineedit
     // cosmic-light: too gray
-    //
-    // Changing the config rebuilds many things; try SLINT_STYLE instead, on Linux
-    // (not an option on Android...)
 
     slint_build::compile_with_config("ui/app-window.slint", config).expect("Slint build failed");
 }
