c78cc1ffff47946c115ec7e03cb3ae2e057c9ec7
David Faure <david.faure@kdab.com>
2026-04-05 21:15:01 +0200

    silence winit tracing coming via log

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,6 +3,10 @@ use std::error::Error;
 use videofinder::videofinder_main;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    // Prevent tracing (used by winit) from forwarding debug spam to the log crate.
+    // Installing any subscriber disables the tracing->log bridge.
+    tracing::subscriber::set_global_default(tracing::subscriber::NoSubscriber::new()).ok();
+
     // Log everything to stderr
     // (I use flexi_logger because on Android I need to log to a file)
     let _logger = flexi_logger::Logger::with(flexi_logger::LevelFilter::Debug).start().unwrap();
